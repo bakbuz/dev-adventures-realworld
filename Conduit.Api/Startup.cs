31ce6029d0b09c6b6193b550c6d81ea57ce92e42
Bayram Akbuz <bayram.akbuz@outlook.com>
2019-10-19 21:23:02 +0300

    api recreated

---
+++ Conduit.Api/Startup.cs
@@ -0,0 +1,88 @@
+using AutoMapper;
+using Conduit.Api.Configuration;
+using Conduit.Api.Filters;
+using Conduit.Api.ModelBinders;
+using Conduit.Business.Identity;
+using Conduit.Business.Services;
+using Conduit.Core.Configuration;
+using Conduit.Core.Identity;
+using Conduit.Core.Services;
+using Conduit.Data.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Conduit.Api
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+              .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext(Configuration.GetConnectionString("DbConnectionString"));
+            services.AddAutoMapper(typeof(Startup));
+            services.AddSwagger();
+            services.AddJwtIdentity(Configuration.GetSection(nameof(JwtConfiguration)));
+
+            services.AddTransient<IUsersService, UsersService>();
+            services.AddTransient<IProfilesService, ProfilesService>();
+            services.AddTransient<IArticlesService, ArticlesService>();
+            services.AddTransient<ITagsService, TagsService>();
+            services.AddTransient<IArticleCommentsService, ArticleCommentsService>();
+            services.AddTransient<IJwtFactory, JwtFactory>();
+
+            services.AddMvcCore(options =>
+            {
+                options.ModelBinderProviders.Insert(0, new OptionModelBinderProvider());
+                options.Filters.Add<ExceptionFilter>();
+                options.Filters.Add<ModelStateFilter>();
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory, ApplicationDbContext dbContext)
+        {
+            loggerFactory.AddLogging(Configuration.GetSection("Logging"));
+
+            if (env.IsDevelopment())
+            {
+                dbContext.Database.EnsureCreated();
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseSwagger("My Web API.");
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
